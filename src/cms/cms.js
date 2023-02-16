a919e8360ce1fb1de2bdd640f1b93ac4d2ce85f5
Andrea Valla <valla.andrea@gmail.com>
2023-02-16 23:36:09 +0100

    wip

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,23 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+import { createLocalizedWidget } from 'netlify-cms-i18n'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+
+const LOCALES = ['en', 'de', 'it']
+
+CMS.getWidgets().forEach((widget) => {
+    const { name, control, preview } = createLocalizedWidget(widget, LOCALES)
+    CMS.registerWidget(name, control, preview)
+})
+
+CMS.init()
